cf54f9eb23a467f3e0fdd6033b94fb5f19fe0970
Måns Isaksson <mans.95.isaksson@hotmail.com>
2016-06-27 17:01:37 +0200

    Sync

--- SecondPersonShooter/Source/SecondPersonShooter/EnemyCharacter.cpp
+++ SecondPersonShooter/Source/SecondPersonShooter/EnemyCharacter.cpp
@@ -118,14 +118,6 @@ float AEnemyCharacter::TakeDamage(float DamageAmount, FDamageEvent const& Damage
 
 		if (Health <= 0)
 		{
-			if (PlayerRef->GetPossessedEnemy() == this)
-			{
-				PlayerRef->PossessedIsKilled();
-			}
-			else
-			{
-				PlayerRef->AddScore((GetScoreValue() / (((GetActorLocation() - PlayerRef->GetActorLocation()).Size() + 10) / 100)));			
-			}
 			if (SPS::GetGameMode(this)->GetCurrentGameMode() == EGameMode::WaveMode && SPS::GetGameMode(this)->GetIsLastInWave())
 				DisableEnemy(5.f, true, true);
 			else
@@ -133,6 +125,11 @@ float AEnemyCharacter::TakeDamage(float DamageAmount, FDamageEvent const& Damage
 				FVector FromAngle = GetActorLocation() - DamageCauser->GetActorLocation();
 				FromAngle.Normalize();
 				KillEnemy(FromAngle * 10000.f);
+
+				if (PlayerRef->GetPossessedEnemy() == this)
+					PlayerRef->PossessedIsKilled();
+				else
+					PlayerRef->AddScore((GetScoreValue() / (((GetActorLocation() - PlayerRef->GetActorLocation()).Size() + 10) / 100)));
 			}
 		}
 		else
